2ff5b727b072c3fd68aae614cc3f90cdbe788a50
fusiontwo <yr0955@naver.com>
2024-03-03 00:52:55 +0900

    .

--- pythonSample.py
+++ pythonSample.py
@@ -2,3 +2,4 @@ print("********************************")
 print("Python has entered the building.")
 print("Let the party start!!!!!!")
 print("********************************")
+# TODO update the detail @fusiontwo 
